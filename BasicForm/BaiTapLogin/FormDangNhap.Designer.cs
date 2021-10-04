e02199d4654b17cf9e75c5f34830dd776c459a8e
Tuong Vy <lenguyentuongvyjh@gmail.com>
2021-10-04 15:41:48 +0700

    importing files

---
+++ BasicForm/BaiTapLogin/FormDangNhap.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace BasicForm.BaiTapLogin
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBoqua = new System.Windows.Forms.Button();
+            this.btDongy = new System.Windows.Forms.Button();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.txtten = new System.Windows.Forms.TextBox();
+            this.lblMatkhau = new System.Windows.Forms.Label();
+            this.lblTenDN = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btBoqua
+            // 
+            this.btBoqua.Location = new System.Drawing.Point(427, 136);
+            this.btBoqua.Name = "btBoqua";
+            this.btBoqua.Size = new System.Drawing.Size(75, 30);
+            this.btBoqua.TabIndex = 14;
+            this.btBoqua.Text = "Bỏ qua";
+            this.btBoqua.UseVisualStyleBackColor = true;
+            // 
+            // btDongy
+            // 
+            this.btDongy.Location = new System.Drawing.Point(336, 136);
+            this.btDongy.Name = "btDongy";
+            this.btDongy.Size = new System.Drawing.Size(75, 30);
+            this.btDongy.TabIndex = 13;
+            this.btDongy.Text = "Đồng ý";
+            this.btDongy.UseVisualStyleBackColor = true;
+            this.btDongy.Click += new System.EventHandler(this.btDongy_Click);
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(169, 87);
+            this.txtmatkhau.Margin = new System.Windows.Forms.Padding(4);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(333, 22);
+            this.txtmatkhau.TabIndex = 12;
+            this.txtmatkhau.UseSystemPasswordChar = true;
+            // 
+            // txtten
+            // 
+            this.txtten.Location = new System.Drawing.Point(169, 39);
+            this.txtten.Margin = new System.Windows.Forms.Padding(4);
+            this.txtten.Name = "txtten";
+            this.txtten.Size = new System.Drawing.Size(333, 22);
+            this.txtten.TabIndex = 11;
+            // 
+            // lblMatkhau
+            // 
+            this.lblMatkhau.AutoSize = true;
+            this.lblMatkhau.Location = new System.Drawing.Point(41, 87);
+            this.lblMatkhau.Name = "lblMatkhau";
+            this.lblMatkhau.Size = new System.Drawing.Size(70, 17);
+            this.lblMatkhau.TabIndex = 10;
+            this.lblMatkhau.Text = "Mật khẩu:";
+            // 
+            // lblTenDN
+            // 
+            this.lblTenDN.AutoSize = true;
+            this.lblTenDN.Location = new System.Drawing.Point(41, 39);
+            this.lblTenDN.Name = "lblTenDN";
+            this.lblTenDN.Size = new System.Drawing.Size(109, 17);
+            this.lblTenDN.TabIndex = 9;
+            this.lblTenDN.Text = "Tên đăng nhập:";
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 204);
+            this.Controls.Add(this.btBoqua);
+            this.Controls.Add(this.btDongy);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.txtten);
+            this.Controls.Add(this.lblMatkhau);
+            this.Controls.Add(this.lblTenDN);
+            this.Name = "FormDangNhap";
+            this.Text = "FormDangNhap";
+            this.Load += new System.EventHandler(this.FormDangNhap_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBoqua;
+        private System.Windows.Forms.Button btDongy;
+        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.TextBox txtten;
+        private System.Windows.Forms.Label lblMatkhau;
+        private System.Windows.Forms.Label lblTenDN;
+    }
+}
---
+++ BasicForm/BaiTapLogin/FormDangNhap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicForm.BaiTapLogin
+{
+    public partial class FormDangNhap : Form
+    {
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btDongy_Click(object sender, EventArgs e)
+        {
+            string id = "ndungithue";
+            string Password = "ABC@123";
+            if (id.Equals(txtten.Text) && Password.Equals(txtmatkhau.Text))
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+                MessageBox.Show("Đăng nhập thất bại");
+        }
+       
+    }
+}
